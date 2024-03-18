f4b571fe4bc368ef078c55f6a77b96ad3d3838b7
Gabrielle Bouchard <gabrielle.bouchard.13@ulaval.ca>
2024-03-18 10:03:26 -0400

    Final

--- db_scripts/migrate_1.sql
+++ db_scripts/migrate_1.sql
@@ -2,10 +2,13 @@ CREATE TABLE IF NOT EXISTS band (bandName VARCHAR(50), creation YEAR, genre varc
 INSERT INTO band (bandName, creation, genre) VALUES ('Crazy Duo', 2015, 'rock'), ('Luna', 2009, 'classical'), ('Mysterio', 2019, 'pop');
 
 ALTER TABLE singer RENAME TO musician;
+
 ALTER TABLE musician RENAME COLUMN singerName TO musicianName;
-ALTER TABLE musician DROP PRIMARY KEY, ADD CONSTRAINT musician.musicianName PRIMARY KEY (musicianName);
+
+ALTER TABLE musician DROP PRIMARY KEY, ADD CONSTRAINT musicianName PRIMARY KEY (musicianName);
 
 ALTER TABLE musician ADD COLUMN role VARCHAR(50), ADD COLUMN brandName VARCHAR(50);
+
 UPDATE musician SET role = 'vocals', brandName = 'Crazy Duo' WHERE musicianName = 'Alina';
 UPDATE musician SET role = 'guitar', brandName = 'Mysterio' WHERE musicianName = 'Mysterio';
 UPDATE musician SET role = 'percussion', brandName = 'Crazy Duo' WHERE musicianName = 'Rainbow';
